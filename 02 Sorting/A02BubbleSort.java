b9d0eebb958d7248e3b60fd08b6883eb0c0264b4
sharkiiee <sarthak231203@gmail.com>
2025-01-01 18:15:36 +0530

    bubble sort program added

--- 02 Sorting/A02BubbleSort.java
+++ 02 Sorting/A02BubbleSort.java
@@ -1,5 +1,25 @@
+
+import java.util.Arrays;
+
 // It pushes the maximum to the last by adjucent swaps
 
 public class A02BubbleSort {
-    
+
+    static int[] sortTheArr(int[] arr){
+        for (int i = arr.length-1; i >= 1; i--) {
+            for (int j = 0; j <= i-1; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int arr[] = {13,46,24,52,20,9};
+        int[] sortedArr = sortTheArr(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(sortedArr));
+    }
 }
