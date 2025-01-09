c45f30560b97d239994ccd00c593e122b05106a6
sharkiiee <sarthak231203@gmail.com>
2025-01-09 13:45:12 +0530

    adding largest file in the dsa4

--- 03 Array/Easy/A01LargestElement.java
+++ 03 Array/Easy/A01LargestElement.java
@@ -7,9 +7,13 @@
 
 public class A01LargestElement {
     public static void main(String[] args) {
-        largest = a[0];
-        for (int i = 0; i < n; i++) {
-            
+        int[] numbers = {10,20,60,40,50};
+        int largest = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i]>largest){
+                largest = numbers[i];
+            }
         }
+        System.out.println(largest);
     }
 }
